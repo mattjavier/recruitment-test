f4e7bf9df81ee70e6a9cab77454d275cfe2f6f20
Matt Javier <matthewrjavier@gmail.com>
2021-01-26 16:36:37 -0800

    added themes

--- full-stack-recruitment-test/pages/_app.js
+++ full-stack-recruitment-test/pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import theme from './theme.js';
+
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
