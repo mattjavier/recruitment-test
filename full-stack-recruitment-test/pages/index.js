1eff6f3964a166e874c4b9dd239ad33ef62da8d7
Matt Javier <matthewrjavier@gmail.com>
2021-01-26 16:50:22 -0800

    move components/ to src/components

--- full-stack-recruitment-test/pages/index.js
+++ full-stack-recruitment-test/pages/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import Header from '../components/Header';
-import Flights from '../components/Flights';
+import Header from '../src/components/Header';
+import Flights from '../src/components/Flights';
 
 export default function Home() {
   return (
